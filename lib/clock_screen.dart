dcdcec94e5186406a2b2a0589a4e2ed69245ee98
jo4s3ph shaman <jo4s3ph@gmail.com>
2024-11-19 22:49:23 +0300

    Cleaning the code
    Clean the startup code and create the clock_screen.dart file

---
+++ lib/clock_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClockScreen extends StatefulWidget {
+  const ClockScreen({super.key});
+
+  @override
+  State<ClockScreen> createState() => _ClockScreenState();
+}
+
+class _ClockScreenState extends State<ClockScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
