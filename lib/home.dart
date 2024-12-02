b0133a9997eb7cd8125b2da34c137ad1c0686f59
jo4s3ph shaman <jo4s3ph@gmail.com>
2024-12-02 21:59:54 +0300

    Dark mode toggle
    User can tap where tapping anywhere on the screen to changes the mode now.
    
    By using GestureDetector widget to wrap the Scaffold widget in home.dart file.

--- lib/home.dart
+++ lib/home.dart
@@ -11,58 +11,68 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   DateTime now = DateTime.now();
+  bool isDarkMode = false;
 
   // Future<DateTime> fetchClock() async {
   //   now = DateTime.now();
   //   return now;
   // }
 
+  changeMode() {
+    setState(() {
+          isDarkMode = !isDarkMode;
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      backgroundColor: const Color(0xFFfafafa),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TimerBuilder.periodic(
-                const Duration(seconds: 1),
-                builder: (context) {
-                  //getting the time
-                  String second = DateTime.now().second < 10
-                      ? "0${DateTime.now().second}"
-                      : DateTime.now().second.toString();
-                  String minute = DateTime.now().minute < 10
-                      ? "0${DateTime.now().minute}"
-                      : DateTime.now().minute.toString();
-                  String hour = DateTime.now().hour < 10
-                      ? "0${DateTime.now().hour}"
-                      : DateTime.now().hour.toString();
-                  // String day = DateTime.now().toString();
-                  return Center(
-                    child: Column(
-                      children: [
-                        Text(
-                          "Today",
-                          style: AppStyle.mainTextThin,
-                        ),
-                        const SizedBox(
-                          height: 40.0,
-                        ),
-                        Text(
-                              "$hour:$minute:$second",
-                              style: AppStyle.maintext,
-                            ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ],
+    return GestureDetector(
+      onTap: () => changeMode(),
+      child: Scaffold(
+        backgroundColor: isDarkMode ? AppStyle.primaryColor : AppStyle.primaryColorDark,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TimerBuilder.periodic(
+                  const Duration(seconds: 1),
+                  builder: (context) {
+                    //getting the time
+                    String second = DateTime.now().second < 10
+                        ? "0${DateTime.now().second}"
+                        : DateTime.now().second.toString();
+                    String minute = DateTime.now().minute < 10
+                        ? "0${DateTime.now().minute}"
+                        : DateTime.now().minute.toString();
+                    String hour = DateTime.now().hour < 10
+                        ? "0${DateTime.now().hour}"
+                        : DateTime.now().hour.toString();
+                    // String day = DateTime.now().toString();
+                    return Center(
+                      child: Column(
+                        children: [
+                          Text(
+                            "Today",
+                            style: isDarkMode? AppStyle.mainTextThin : AppStyle.mainTextThinDark,
+                          ),
+                          const SizedBox(
+                            height: 40.0,
+                          ),
+                          Text(
+                                "$hour:$minute:$second",
+                                style: isDarkMode? AppStyle.maintext : AppStyle.maintextDark,
+                              ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/style/colors.dart
+++ lib/style/colors.dart
@@ -4,14 +4,29 @@ class AppStyle {
   // static Color primaryColor = const Color(0xFF2253FF);
   // static Color primaryColorDark = const Color(0xFF193BB1);
 
+
+  static Color primaryColor = const Color(0xFFfafafa);
+  static Color primaryColorDark = const Color.fromARGB(255, 17, 25, 54);
   /* Definining text style */
   static TextStyle maintext = const TextStyle(
-    color: Colors.cyan,
-    fontSize: 78.0,
+    color: Color.fromARGB(255, 17, 25, 54),
+    fontSize: 82.0,
     fontWeight: FontWeight.bold,
   );
+  static TextStyle maintextDark = const TextStyle(
+    color: Color.fromARGB(255, 213, 216, 47),
+    fontSize: 82.0,
+    fontWeight: FontWeight.bold,
+  );
+
+
   static TextStyle mainTextThin = const TextStyle(
     color: Colors.deepOrange,
+    fontSize: 32.0,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle mainTextThinDark = const TextStyle(
+    color: Color.fromARGB(255, 241, 215, 207),
     fontSize: 32.0,
     fontWeight: FontWeight.normal,
   );
